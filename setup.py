69c7c1180028d0afb7e3fdd4ea4edaac4509b036
Leo Honkanen <leo.o.honkanen@gmail.com>
2011-07-10 23:45:51 +0300

    update version for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='lyra',
-      version='1.0',
+      version='1.1',
       description='time management app for django',
       author='Leo Honkanen',
       author_email='leo.o.honkanen@gmail.com',
