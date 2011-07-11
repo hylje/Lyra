8750a56ac9b5d17af17fe68e4d6293537dd5cb96
Leo Honkanen <leo.o.honkanen@gmail.com>
2011-07-11 11:40:01 +0300

    Elaborate README and version guard Django

--- lyra/__init__.py
+++ lyra/__init__.py
@@ -1,3 +1,10 @@
+import django
+
+major, minor, patch, _, _ = django.VERSION
+
+if major == 1 and minor < 3:
+    raise Exception("Lyra requires Django 1.3 or later. Currently installed: %s.%s" % (major, minor))
+
 from lyra import views
 
 root = views.Lyra()
